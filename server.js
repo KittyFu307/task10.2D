32f037689c6d82b67f9dcd14e0811a88ad07e0a4
Kitty <fushikun307@outlook.com>
2020-10-08 22:39:40 +0800

    first commit

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const bodyParser = require("body-parser")
 const User = require("./models/User")
 const mongoose = require("mongoose")
 const https = require("https")
-
+const fileUpload = require('express-fileupload');
 const cors = require("cors")
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 app.use(bodyParser.json())
+
+app.use(fileUpload());
 
 mongoose.connect("mongodb+srv://admin-kitty:Deakin2020@cluster0.pzwde.mongodb.net/Task?retryWrites=true&w=majority", {useNewUrlParser: true ,useUnifiedTopology:true})
 
@@ -34,9 +36,25 @@ app.post('/register',(req,res)=>{
     .catch((err)=>console.log(err));
     res.json('save to db:' + user)
 })
+
+app.post('/upload',(req,res)=>{
+    if(req.files === null){
+        return res.status(400).json({msg:"No file upload"});
+    }
+    const file = req.files.file
+
+    file.mv(`${__dirname}/task10/public/uploads/${file.name}`, err =>{
+        if(err){
+            console.error(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({fileName:file.name, filePath: `/uploads/${file.name}`});
+    })
+})
 let port = process.env.PORT;
 if(port == null || port == ""){
-    port = 8000;
+    port = 4000;
 }
 app.listen(port, (req,res)=>{
     console.log("Server is running successfully!")
